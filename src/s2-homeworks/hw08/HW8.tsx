2ee5fb244e387c8e442a04e1b400594905653cca
Daniil <daniilminin1990@yandex.ru>
2024-02-26 17:42:40 +0600

    hw5-hw8 done

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
+import React, { useState } from 'react'
+import { homeWorkReducer } from './bll/homeWorkReducer'
 import s from './HW8.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
@@ -19,36 +19,36 @@ export type UserType = {
 
 const initialPeople: UserType[] = [
     // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+    { _id: 0, name: 'Кот', age: 3 },
+    { _id: 1, name: 'Александр', age: 66 },
+    { _id: 2, name: 'Коля', age: 16 },
+    { _id: 3, name: 'Виктор', age: 44 },
+    { _id: 4, name: 'Дмитрий', age: 40 },
+    { _id: 5, name: 'Ирина', age: 55 },
 ]
 
 const HW8 = () => {
     const [people, setPeople] = useState<UserType[]>(initialPeople)
     const [currentSort, setCurrentSort] = useState('')
 
-    const finalPeople = people.map((u: UserType) => <User key={u._id} u={u}/>)
+    const finalPeople = people.map((u: UserType) => <User key={u._id} u={u} />)
 
     const sortUp = () => {
         setPeople(
-            homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'})
+            homeWorkReducer(initialPeople, { type: 'sort', payload: 'up' })
         ) // в алфавитном порядке a.name > b.name
         setCurrentSort('up')
     }
 
     const sortDown = () => {
         setPeople(
-            homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'})
+            homeWorkReducer(initialPeople, { type: 'sort', payload: 'down' })
         ) // в обратном порядке a.name < b.name}
         setCurrentSort('down')
     }
     const check18 = () => {
         setPeople(
-            homeWorkReducer(initialPeople, {type: 'check', payload: 18})
+            homeWorkReducer(initialPeople, { type: 'check', payload: 18 })
         ) // совершеннолетние
         setCurrentSort('18')
     }
@@ -84,10 +84,10 @@ const HW8 = () => {
 
                     <table id={'hw8-users'} className={s.users}>
                         <thead className={s.thead}>
-                        <tr>
-                            <td className={s.nameCol}>Name</td>
-                            <td className={s.ageCol}>Age</td>
-                        </tr>
+                            <tr>
+                                <td className={s.nameCol}>Name</td>
+                                <td className={s.ageCol}>Age</td>
+                            </tr>
                         </thead>
 
                         <tbody>{finalPeople}</tbody>
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,29 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const a = action.payload
+            // const copyState = state.map(u => u)
+            // return a === 'up'
+            //     ? copyState.sort((x, y) => x.name.localeCompare(y.name))
+            //     : copyState.sort((x, y) => y.name.localeCompare(x.name))
+            const copyUp = [...state].sort((x, y) => {
+                if (x.name > y.name) return 1
+                if (x.name < y.name) return -1
+                return 0
+            })
+            const copyDown = [...state].sort((x, y) => {
+                if (x.name < y.name) return 1
+                if (x.name > y.name) return -1
+                return 0
+            })
+            return a === 'up' ? copyUp : copyDown
         }
         case 'check': {
-
-            return state // need to fix
+            const a = action.payload
+            return state.filter(u => u.age>=a && u)
         }
         default:
             return state
