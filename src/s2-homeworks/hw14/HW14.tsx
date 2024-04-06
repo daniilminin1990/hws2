a6b846c77bd1709f2aef635a69d0784e3a8d9f9f
Daniil <daniilminin1990@yandex.ru>
2024-04-06 19:01:14 +0500

    14 5-2 DONE

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -64,7 +64,7 @@ const HW14 = () => {
     const params = Object.fromEntries(searchParams)
     sendQuery(params.find || '')
     setFind(params.find || '')
-  }, [searchParams])
+  }, [])
 
   const mappedTechs = techs.map(t => (
     <div key={t} id={'hw14-tech-' + t} className={s.tech}>
